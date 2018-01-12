6d9e1ac1503be8a1159cbdcc247f2f031e60cc84
Rodrigo Parada <rpparada@gmail.com>
2018-01-12 15:59:40 -0300

    Comienzo Unidad JavaScript

---
+++ JavaScript_Level_One/Part3_solucion.js
@@ -0,0 +1,4 @@
+var libras = prompt("Dame tu peso en libras, yo lo cambiaré a kilos:");
+var kilos = libras * 0.45359237;
+alert("Tu peso en kilos es: " + kilos);
+//alert("Tu peso en kilos es: " + kilos.toString(kilos));
---
+++ JavaScript_Level_One/myscript.js
@@ -0,0 +1,4 @@
+alert("Hola Mundo");
+var mensaje = prompt("Escribe algo:");
+alert("Tu mensaje es: " + mensaje);
+console.log("Mensaje en consola: " + mensaje);
