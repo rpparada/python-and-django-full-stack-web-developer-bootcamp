39ce5e7477133f34a6ff90a6604035bd323a3082
Rodrigo Parada <rpparada@gmail.com>
2018-01-26 15:32:56 -0300

    Django Unidad - Trabajando con MTV
    Extraer data y cargarlas en la web

--- Django_Clases/primer_proyecto/primera_app/views.py
+++ Django_Clases/primer_proyecto/primera_app/views.py
@@ -3,9 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from primera_app.models import AccessRecord, Webpage, Topic
 
 # Create your views here.
 def index(request):
-    my_dict = {'insert_me':'Hola, estoy desde primera_app/index.html'}
-    return render(request,'primera_app/index.html',context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records':webpages_list}
+    # my_dict = {'insert_me':'Hola, estoy desde primera_app/index.html'}
+    return render(request,'primera_app/index.html',context=date_dict)
     # return HttpResponse("Hola Mundo!")
