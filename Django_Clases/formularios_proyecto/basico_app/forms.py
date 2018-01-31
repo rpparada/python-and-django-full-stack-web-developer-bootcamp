00d6e70356e71dcfbeee27bc0e5aa36986fd26b8
Rodrigo Parada <rpparada@gmail.com>
2018-01-31 16:54:57 -0300

    Django Unidad - Formularios

--- Django_Clases/formularios_proyecto/basico_app/forms.py
+++ Django_Clases/formularios_proyecto/basico_app/forms.py
@@ -1,6 +1,14 @@
 from django import forms
+from django.core import validators
 
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required=False,widget=forms.HiddenInput,validators=[validators.MaxLengthValidator(0)] )
+
+    # def clean_botcatcher(self):
+    #     botcatcher = self.cleaned_data['botcatcher']
+    #     if len(botcatcher) > 0:
+    #         raise forms.ValidationError("BOT FOUND!!")
+    #     return botcatcher
