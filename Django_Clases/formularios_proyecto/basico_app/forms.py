e067101afef735d672a67e536ae03e8f6835c8fe
Rodrigo Parada <rpparada@gmail.com>
2018-02-04 23:03:10 -0300

    Django Unidad - Fin Validacion formularios
    Sin Ejercicio final

--- Django_Clases/formularios_proyecto/basico_app/forms.py
+++ Django_Clases/formularios_proyecto/basico_app/forms.py
@@ -1,11 +1,25 @@
 from django import forms
 from django.core import validators
 
+# def verifica_primera_letra_z(value):
+#     if value[0].lower() != 'z':
+#         raise forms.ValidationError('Nombre tiene que comenzar con z')
+
 class FormName(forms.Form):
     name = forms.CharField()
+    # name = forms.CharField(validators=[verifica_primera_letra_z])
     email = forms.EmailField()
+    verifica_email = forms.EmailField(label='Ingresa tu email nuevamente:')
     text = forms.CharField(widget=forms.Textarea)
-    botcatcher = forms.CharField(required=False,widget=forms.HiddenInput,validators=[validators.MaxLengthValidator(0)] )
+
+    def clean(self):
+        all_clean_data = super(forms.Form,self).clean()
+        email = all_clean_data['email']
+        vemail = all_clean_data['verifica_email']
+
+        if email != vemail:
+            raise forms.ValidationError("Ambos correos deben ser iguales")
+    # botcatcher = forms.CharField(required=False,widget=forms.HiddenInput,validators=[validators.MaxLengthValidator(0)] )
 
     # def clean_botcatcher(self):
     #     botcatcher = self.cleaned_data['botcatcher']
