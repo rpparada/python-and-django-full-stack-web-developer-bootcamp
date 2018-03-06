f29d2009d7b0f3713960b4a3fbb527ca03c7c88b
Rodrigo Parada <rpparada@gmail.com>
2018-03-05 21:06:31 -0300

    Django - Blog - Parte 3

---
+++ blog_project/miSitioDjango/blog/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from blog.models import Comment,Post
+
+class PastForm(forms.ModelForm):
+
+    class Meta():
+        model = Post
+        fields = ('author','title','text')
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'}),
+        }
+class CommentForm(forms.ModelForm):
+
+    class Meta():
+        model = Comment
+        fields = ('author','text')
+
+        widgets = {
+            'auther':forms.TextInput(attrs={¡'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea'}),
+        }
---
+++ blog_project/miSitioDjango/blog/models.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = modelsDateTimeFiend(blank=True,null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filetr(approved_comment=True)
+
+    def get_absolute_url(self):
+        return reverse('post_detail',kwagrs={'pk':self.pk})
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post',related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanFiend(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.text
